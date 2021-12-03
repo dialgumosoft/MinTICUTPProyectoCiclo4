afd0085affe5afcbe739cfa068947bf8868c3b27
IvanOconBarrios <ivanoconbarrios@gmail.com>
2021-12-03 08:47:44 -0500

    Merge branch 'master' of https://github.com/dialgumosoft/Web-02-Grupo-5

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Nav from "../../components/nav/nav";
 import Footer from "../../components/footer/footer";
-import Carousel from '../../components/carousel/carousel';
-
+import Carousel from "../../components/carousel/carousel";
 
 function Home(params) {
   return (
     <>
       <div className="d-flex h-100 text-dark">
-        <Nav/>
-          <div className="cover-container d-flex w-100 h-100 p-0 mx-auto flex-column">               
-            <Carousel/>
-          </div>
-        <Footer/>
+        <Nav />
+        <div className="cover-container d-flex w-100 h-100 p-0 mx-auto flex-column">
+          <Carousel />
+        </div>
+      </div>
+      <div>
+        <Footer />
       </div>
     </>
   );
